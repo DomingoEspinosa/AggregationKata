1a450d32fd8b5fa610f3db2f20ee9aca04215aaa
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-08 10:22:25 +0200

    Refactor

--- src/test/java/Studentshould.java
+++ src/test/java/Studentshould.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,16 +9,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Studentshould {
+
+    private Student jane;
+    private Student galina;
+    private Student anton;
+    private Student jack;
+    private Student mike;
+
+    @BeforeEach
+    void setUp() {
+        jane = new Student("Jane", 65, "CS", "Female");
+        galina = new Student("Galina", 95, "Philology", "Female");
+        anton = new Student("Anton", 90, "CS", "Male");
+        jack = new Student("Jack", 82, "Philology", "Male");
+        mike = new Student("Mike", 60, "Philology", "Male");
+    }
+
     @Test
     public void basicTestGetAverageGradeByDepartment() {
 
-        Student galina = new Student("Galina", 95, "Philology", "Female");
-        Student anton = new Student("Anton", 90, "CS", "Male");
-        Student jack = new Student("Jack", 82, "Philology", "Male");
-        Student mike = new Student("Mike", 60, "Philology", "Male");
-        Student jane = new Student("Jane", 65, "CS", "Female");
         Student[] students = new Student[]{galina, anton, jack, mike, jane};
-        
+
         Map<String, Double> expectedGrade = Student.getAverageGradeByDepartment(Arrays.stream(students));
         Map<String, Double> actualGrade = new HashMap<>();
         actualGrade.put("CS", 77.5);
